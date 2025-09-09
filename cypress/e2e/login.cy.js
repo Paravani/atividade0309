3c538d938bc4d3452d3385e13aeccfff6a7b8c8b
Paravani <mparavani6@gmail.com>
2025-09-09 20:17:54 -0300

    comitado

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,30 @@
-describe('Deve fazer login na conta', () => {
-  it('Deve fazer o login na conta', ()=> {
+describe("Tela de login", function () {
+  beforeEach(function () {
+    cy.visit("https://forum.adrenaline.com.br");
+    cy.contains("Log in").click(); // Corrigido para abrir o modal de login
+  });
 
-    cy.visit('https://idp.azerionconnect.com/auth/realms/spil/protocol/openid-connect/auth?client_id=39c6f0dd-0ace-42db-aa86-b9dd1907764a&extra_data&redirect_uri=https%3A%2F%2Fwww.gamesgames.com%2Fusers%2Fauth%2Fgamesgamesconnect%2Fcallback&response_type=code&state=6e3438317c96556ae9b5431de459dfca0ba8875683a30425')
-    
-    cy.get('#username').type('a3d0d5f3b1@webxios.pro')
+  it("Deve entrar com o login ", function () {
+    cy.get('input[name="login"]').type("jogino9459@skateru.com");
+    cy.get('input[name="password"]').type("123456teste");
+    cy.get('button[class="button button--icon button--icon--login button--primary"]').click();
 
-    cy.get('#password').type('Senha1234')
-    
-    cy.get('button[type="submit"][name="login"]').click()
+  });
 
-    cy.url().should('include', 'gamesgames.com/')
-   
+  it("Deve falhar o login com e-mail errado", function () {
+    cy.get('input[name="login"]').type("usuario_errado@skateru.com");
+    cy.get('input[name="password"]').type("123456teste");
+    cy.get('button[class="button button--icon button--icon--login button--primary"]').click();
+
+    cy.contains("O usuário solicitado 'usuario_errado@skateru.com' não pode ser encontrado.").should("be.visible");
+  });
+
+
+  it("Deve falhar o login com a senha errado", function () {
+    cy.get('input[name="login"]').type("jogino9459@skateru.com");
+    cy.get('input[name="password"]').type("123456");
+    cy.get('button[class="button button--icon button--icon--login button--primary"]').click();
+
+    cy.contains("Senha errada. Tente novamente.").should("be.visible");
   });
 });
